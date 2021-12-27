878e29909b8c1af5ded863588cc190b57310bca5
VictorNanka <VictorNanka@gmail.com>
2021-12-27 19:04:13 +0800

    update: add header and footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,23 @@
-module.exports = function (eleventyConfig) {
+// Import data files
+const site = require("./src/_data/site.js");
+
+module.exports = function (config) {
+  // Passthrough
+  config.addPassthroughCopy({ "src/static": "/" });
+
+  // Watch for changes to my source files
+  if (config.addWatchTarget) {
+    config.addWatchTarget("/src/scss");
+    config.addWatchTarget("/src/js");
+  } else {
+    console.log("No additional watch targets");
+  }
+
   return {
     dir: {
       input: "src",
       output: "_site",
     },
+    passthroughFileCopy: true,
   };
 };
---
+++ src/_data/site.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "NLP on Browser",
+  shortDesc:
+    "This is a project to practice NLP on the browser. It's a simple web app that allows you dig data from social network and get the NLP analysis of it.",
+  url: "",
+  authorEmail: "",
+  authorHandle: "",
+  authorName: "",
+
+  theme: {
+    background: "black",
+    text: "white",
+  },
+};
