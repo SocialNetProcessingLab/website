99e14b9d5328be25d2d315510cdfc125f66bdd49
VictorNanka <VictorNanka@gmail.com>
2021-12-28 16:52:45 +0800

    build: add tailwindcss support

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,12 @@ const site = require("./src/_data/site.js");
 
 module.exports = function (config) {
   // Passthrough
-  config.addPassthroughCopy({ "src/static": "/" });
+  config.addPassthroughCopy("./src/sytles/styles.css");
 
   // Watch for changes to my source files
   if (config.addWatchTarget) {
-    config.addWatchTarget("/src/scss");
-    config.addWatchTarget("/src/js");
+    config.addWatchTarget("./src/styles");
+    config.addWatchTarget("./src/js");
   } else {
     console.log("No additional watch targets");
   }
